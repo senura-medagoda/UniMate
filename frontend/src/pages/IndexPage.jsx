7759dacf6172b7e720c3557c3931c8463f4164d0
senura-medagoda <medagoda.senura@gmail.com>
2025-08-15 00:28:41 +0530

    Navbar

--- frontend/src/pages/IndexPage.jsx
+++ frontend/src/pages/IndexPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './landingComponents/Navbar'
 
 const index = () => {
   return (
-    <div>
+    <div className='min-h-screen'> 
+    <Navbar/>
       
     </div>
   )
