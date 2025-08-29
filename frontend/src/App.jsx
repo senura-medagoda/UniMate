8321d5c26c6ba132e0a29b9dd68dd15acea7de77
senura-medagoda <medagoda.senura@gmail.com>
2025-08-29 17:20:04 +0530

    JP Init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
 import IndexPage from "./pages/IndexPage.jsx"
+import JP_index from "./pages/StudentUI/JobPortal/JP_index.jsx"
+import JP_jobs from './pages/StudentUI/JobPortal/JP_jobs.jsx'
 
 const App = () => {
   return (
-    <div data-theme="emerald">
+    <div data-theme="emerald" className="relative h-full w-full">
     
       <Routes>
         <Route path="/" element={<IndexPage/>}/>
+        <Route path="/jobdash" element = {<JP_index/>}/>
+        <Route path="/jobs" element = {<JP_jobs/>}/>
       </Routes>
     </div>
   )
